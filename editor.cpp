5ea2776ed445c5f5c1c49bad0cf83f56d0310c9a
Baron Leonardo <mohamedayman.fcis@zoho.com>
2018-02-13 16:27:46 +0200

    create `editor` class based on `QTextEdit`

---
+++ editor.cpp
@@ -0,0 +1,9 @@
+#include "editor.h"
+
+Editor::Editor() {
+
+}
+
+Editor::~Editor() {
+
+}
---
+++ editor.h
@@ -0,0 +1,13 @@
+#ifndef EDITOR_H
+#define EDITOR_H
+
+#include <QTextEdit>
+
+class Editor : public QTextEdit
+{
+public:
+    Editor();
+    ~Editor();
+};
+
+#endif // EDITOR_H

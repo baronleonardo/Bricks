b631ea3833474119b586293d2d5cdf2ea15fff84
Baron Leonardo <mohamedayman.fcis@zoho.com>
2018-02-14 09:17:07 +0200

    create 2 functions - `open` to edit a file, `save` to save changes

--- editor.cpp
+++ editor.cpp
@@ -1,8 +1,46 @@
 #include "editor.h"
 
-Editor::Editor(QWidget *parent) : QTextEdit(parent) {
+Editor::Editor(QWidget *parent) :
+    QTextEdit(parent) {
+    this->open("/tmp/main.cpp");
+}
+
+void Editor::save() {
+    if(!stream) return;
+
+    // TODO: optimization
+    // delete the old text first
+    file->resize(0);
+    *stream << this->toPlainText();
+    stream->flush();
+}
+
+bool Editor::prepare_file(QString path) {
+    // if there was an old opened file
+    if(file) delete file;
+    if(stream) delete stream;
+
+    this->file = new QFile(path);
+    if( !this->file->open(QIODevice::ReadWrite | QIODevice::Text) )
+        return false;
+
+    stream = new QTextStream(file);
+
+    return true;
+}
+
+void Editor::open(QString path) {
+    if(!prepare_file(path))
+        return;
+
+    // clear textedit widget if there is old text
+    if(!this->toPlainText().isEmpty())
+        this->clear();
+
+    this->setText(this->stream->readAll());
 }
 
 Editor::~Editor() {
-
+    if(file) delete file;
+    if(stream) delete stream;
 }
--- editor.h
+++ editor.h
@@ -3,12 +3,26 @@
 
 #include <QTextEdit>
 #include <QWidget>
+#include <QTextStream>
+#include <QString>
+#include <QFile>
 
 class Editor : public QTextEdit
 {
+    Q_OBJECT
+
 public:
-    Editor(QWidget* parent);
+    Editor(QWidget* parent = NULL);
     ~Editor();
+
+private:
+    QFile* file = NULL;
+    QTextStream *stream = NULL;
+    bool prepare_file(QString path);
+
+public slots:
+    void save();
+    void open(QString path);
 };
 
 #endif // EDITOR_H
